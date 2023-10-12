ede5d037b68f804f48a802bb99e5584736e906ba
werq183 <97183851+werq183@users.noreply.github.com>
2023-10-12 19:30:43 +0200

    Fix: change models names in admin panel

--- app/static/js/slider.js
+++ app/static/js/slider.js
@@ -1,27 +1,32 @@
 var currentIndex = 0;
-var images = document.querySelectorAll("#slideshow img");
-var prevBtn = document.getElementById("prevBtn");
-var nextBtn = document.getElementById("nextBtn");
+    var images = document.querySelectorAll("#slideshow img");
+    var prevBtn = document.getElementById("prevBtn");
+    var nextBtn = document.getElementById("nextBtn");
 
-function showImage(index) {
-    images.forEach(function (image) {
-        image.style.display = "none";
+    function showImage(index) {
+        images.forEach(function (image) {
+            image.classList.remove("active");
+        });
+        images[index].classList.add("active");
+    }
+
+    function prevImage() {
+        currentIndex = (currentIndex - 1 + images.length) % images.length;
+        showImage(currentIndex);
+    }
+
+    function nextImage() {
+        currentIndex = (currentIndex + 1) % images.length;
+        showImage(currentIndex);
+    }
+
+    prevBtn.addEventListener("click", prevImage);
+    nextBtn.addEventListener("click", nextImage);
+
+    // Automatyczna zmiana zdjęć co kilka sekund
+    setInterval(nextImage, 5000); // Zmiana co 5 sekund
+
+    // Wywołanie funkcji showImage po załadowaniu strony
+    window.addEventListener("load", function () {
+        showImage(currentIndex);
     });
-    images[index].style.display = "block";
-}
-
-function prevImage() {
-    currentIndex = (currentIndex - 1 + images.length) % images.length;
-    showImage(currentIndex);
-}
-
-function nextImage() {
-    currentIndex = (currentIndex + 1) % images.length;
-    showImage(currentIndex);
-}
-
-prevBtn.addEventListener("click", prevImage);
-nextBtn.addEventListener("click", nextImage);
-
-// Automatyczna zmiana zdjęć co kilka sekund
-setInterval(nextImage, 3000); // Zmiana co 3 sekund
